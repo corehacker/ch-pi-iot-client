0eb71aefe7c77aafca676e1a130c818b87d7c214
Sandeep Prakash <123sandy@gmail.com>
2016-05-17 22:43:12 -0700

    Skeleton code for discover devices.

--- mobile/src/main/java/com/bangaloretalkies/iot/DiscoverActivity.java
+++ mobile/src/main/java/com/bangaloretalkies/iot/DiscoverActivity.java
@@ -1,13 +1,119 @@
 package com.bangaloretalkies.iot;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Enumeration;
 
 public class DiscoverActivity extends AppCompatActivity {
+    private static final String TAG = "DiscoverActivity";
+    private Button refreshButton;
+
+    private void sendDiscoverRequest(DatagramSocket socket) throws IOException {
+        String data = String.format("discover");
+
+        DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(),
+                getBroadcastAddress(), 8080);
+        socket.send(packet);
+    }
+
+    public static InetAddress getBroadcastAddress() throws SocketException {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        for (Enumeration<NetworkInterface> niEnum = NetworkInterface.getNetworkInterfaces(); niEnum.hasMoreElements();) {
+            NetworkInterface ni = niEnum.nextElement();
+            if (!ni.isLoopback()) {
+                for (InterfaceAddress interfaceAddress : ni.getInterfaceAddresses()) {
+                    Log.e(TAG, "Broadcast Address: " + interfaceAddress.getBroadcast());
+
+                    if (null != interfaceAddress.getBroadcast())
+                        return interfaceAddress.getBroadcast();
+                }
+            }
+        }
+        return null;
+    }
+
+    private void listenForResponses(DatagramSocket socket) throws IOException {
+        byte[] buf = new byte[1024];
+        try {
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                String s = new String(packet.getData(), 0, packet.getLength());
+                Log.d(TAG, "Received response " + s);
+
+                //if (null != textView) {
+                //textView.setText(s.toCharArray(), 0, s.length());
+                //}
+
+                //String tagName = "android:switcher:" + R.id.pager + ":" + 1;
+                //Log.d(TAG, "Tag Name: " + tagName);
+                //OperateFragment f2 = (OperateFragment) getActivity().getSupportFragmentManager().findFragmentByTag(tagName);
+                //f2.updateIp(s.split("|").toString().split(":").toString());
+
+
+                String[] hosts = s.split(":");
+                //String[] host1params = hosts[0].split(":");
+
+                String[] port = hosts[1].split("/");
+
+                Log.d(TAG, "IP: " + hosts[0]);
+                Log.d(TAG, "Port: " + port[0]);
+                //f2.updateIp(hosts[0]);
+                //f2.updatePort(port[0]);
+                break;
+            }
+        } catch (SocketTimeoutException e) {
+            Log.d(TAG, "Receive timed out");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+
+        refreshButton = (Button) this.findViewById(R.id.refreshbutton);
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "Discover devices button clicked.");
+                // new MyTask().execute();
+            }
+        });
     }
+
+    public class MyTask extends AsyncTask<Boolean, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Boolean... isChecked) {
+
+            try {
+                DatagramSocket discoverSocket = new DatagramSocket();
+                discoverSocket.setBroadcast(true);
+                discoverSocket.setSoTimeout(5000);
+
+                sendDiscoverRequest (discoverSocket);
+                listenForResponses(discoverSocket);
+                discoverSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not send discovery request", e);
+            }
+
+            return null;
+        }
+    }
+
 }
